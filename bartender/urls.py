1aeef2b74ce1e3f32141d5610c34513b7ac2935e
Aurelien ROUGEMONT <beorn@gandi.net>
2016-07-21 00:05:47 +0200

    redirect / to /characters for now

--- bartender/urls.py
+++ bartender/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
+from django.views.generic import RedirectView
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('characters.urls')),
-    url(r'^', include('django.contrib.auth.urls'))
+    url(r'^', include('django.contrib.auth.urls')),
+    url(r'^$', RedirectView.as_view(url='/characters/'))
 ]
